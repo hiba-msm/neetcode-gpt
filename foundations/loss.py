aa429fa894ba5246ca9e8a34a3eed1c100a9aa07
hiba-msm <msalemhiba@gmail.com>
2026-06-02 10:20:19 +0100

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -5,31 +5,27 @@ from numpy.typing import NDArray
 class Solution:
 
     def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
-        # y_true: true labels (0 or 1)
-        # y_pred: predicted probabilities
-        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
-        # return round(your_answer, 4)
         epsilon = 1e-7
         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
 
-        loss = -np.mean(
-            y_true * np.log(y_pred) +
-            (1 - y_true) * np.log(1 - y_pred)
-        )
+        correct_probs = np.where(y_true == 1, y_pred, 1 - y_pred)
+
+        loss = -np.mean(np.log(correct_probs))
 
         return round(float(loss), 4)
         pass
 
     def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
-        # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
-        # y_pred: predicted probabilities (shape: n_samples x n_classes)
-        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
-        # return round(your_answer, 4)
+
         epsilon = 1e-7
 
         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
 
-        loss = -(np.mean(np.sum(y_true * np.log(y_pred), axis = 1)))
+        true_class_indices = np.argmax(y_true, axis=1)
 
-        return round(loss, 4)
+        correct_probs = y_pred[np.arange(y_true.shape[0]), true_class_indices]
+
+        loss = -np.mean(np.log(correct_probs))
+        return round(float(loss), 4)
+
         pass
