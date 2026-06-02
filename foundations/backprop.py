a3f5b51750614fe256f56c0e9c057f9e40e901eb
hiba-msm <msalemhiba@gmail.com>
2026-06-02 12:02:31 +0100

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -5,27 +5,18 @@ from typing import Tuple
 
 class Solution:
     def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
-        # x: 1D input array
-        # w: 1D weight array
-        # b: scalar bias
-        # y_true: true target value
-        #
-        # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
-        # Loss: L = 0.5 * (y_hat - y_true)^2
-        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
-        # Forward pass
-        z = float(np.dot(x, w)) + b
+
+        z = float(x.dot(w) + b)
+
         y_hat = 1.0 / (1.0 + math.exp(-z))
 
-        # Chain rule
-        dL_dyhat = y_hat - y_true
-        dyhat_dz = y_hat * (1.0 - y_hat)
+        dL_dz = (y_hat - y_true) * y_hat * (1.0 - y_hat)
 
-        dL_dz = dL_dyhat * dyhat_dz
+        dL_dw = np.empty_like(x)
+        np.multiply(x, dL_dz, out=dL_dw)
+        np.round(dL_dw, 5, out=dL_dw)
 
-        # Gradients
-        dL_dw = dL_dz * x
-        dL_db = dL_dz
+        dL_db = round(dL_dz, 5)
 
-        return np.round(dL_dw, 5), round(float(dL_db), 5)
+        return dL_dw, dL_db
         pass
