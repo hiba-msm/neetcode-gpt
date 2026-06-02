201198ab62e16a861ea208502c24a2e749b51196
hiba-msm <msalemhiba@gmail.com>
2026-06-02 10:22:59 +0100

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -6,11 +6,10 @@ class Solution:
 
     def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
         epsilon = 1e-7
-        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-
-        correct_probs = np.where(y_true == 1, y_pred, 1 - y_pred)
-
-        loss = -np.mean(np.log(correct_probs))
+        loss = -np.sum(
+            y_true * np.log(y_pred + epsilon) +
+            (1 - y_true) * np.log(1 - y_pred + epsilon)
+        ) / y_true.size
 
         return round(float(loss), 4)
         pass
@@ -19,13 +18,9 @@ class Solution:
 
         epsilon = 1e-7
 
-        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        loss = -np.sum(
+            y_true * np.log(y_pred + epsilon)
+        ) / y_true.shape[0]
 
-        true_class_indices = np.argmax(y_true, axis=1)
-
-        correct_probs = y_pred[np.arange(y_true.shape[0]), true_class_indices]
-
-        loss = -np.mean(np.log(correct_probs))
         return round(float(loss), 4)
-
         pass
